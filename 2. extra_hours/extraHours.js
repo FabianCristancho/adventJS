4df3d14263ed9329174823754960698030cd8ed1
FCristanchoBOCC <fcristancho@bancodeoccidente.com.co>
2022-12-02 16:18:54 -0500

    Solving second challenge named 'extra Hours'

--- 2. extra_hours/extraHours.js
+++ 2. extra_hours/extraHours.js
@@ -4,9 +4,13 @@ function countHours(year, holidays) {
         let dateInput = holiday.split('/');
         holyDate = new Date(year, parseInt(dateInput[0])-1, parseInt(dateInput[1]))
         if(![0,6].includes(holyDate.getDay())){
-            console.log('entrando...');
             countHours++;
         }
     });
     return countHours*2;
 }
+
+const year = 2022
+const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
+
+console.log(countHours(year, holidays))
